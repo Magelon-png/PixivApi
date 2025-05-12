67ad1f6ee33a8b0a5c7dd7700b755eef5502c201
Magelon-png <52295736+Magelon-png@users.noreply.github.com>
2025-05-12 21:08:20 +0200

    Add methods for endpoints "/v2/illust/mypixiv", "/v1/illust/new" and "/v1/illust/popular"

---
+++ PixivApi/V2/Illust/IllustrationContentType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NetEscapades.EnumGenerators;
+
+namespace Scighost.PixivApi.V2.Illust;
+
+[EnumExtensions]
+public enum IllustrationContentType
+{
+    [Display(Name = "illust")]
+    Illust,
+    [Display(Name = "manga")]
+    Manga,
+    [Display(Name = "novel")]
+    Novel,
+    [Display(Name = "user")]
+    User
+}
